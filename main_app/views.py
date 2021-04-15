cb327a6ee62d050b3ffbb546437cff1effdc394a
Jim Clark <jim.clark@generalassemb.ly>
2021-04-15 10:36:28 -0700

    Add signup

--- main_app/views.py
+++ main_app/views.py
@@ -4,6 +4,8 @@ import boto3
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from .models import Cat, Toy, Photo
 from .forms import FeedingForm
 
@@ -33,6 +35,11 @@ def cats_detail(request, cat_id):
 class CatCreate(CreateView):
   model = Cat
   fields = ['name', 'breed', 'description', 'age']
+
+  def form_valid(self, form):
+    # Assign the logged in user's id to the cat's user
+    form.instance.user = self.request.user
+    return super().form_valid(form)
 
 class CatUpdate(UpdateView):
   model = Cat
@@ -68,9 +75,6 @@ def add_photo(request, cat_id):
       print('An error occurred uploading file to S3')
   return redirect('detail', cat_id=cat_id)
 
-
-
-
 def assoc_toy(request, cat_id, toy_id):
   Cat.objects.get(id=cat_id).toys.add(toy_id)
   return redirect('detail', cat_id=cat_id)
@@ -96,3 +100,19 @@ class ToyUpdate(UpdateView):
 class ToyDelete(DeleteView):
   model = Toy
   success_url = '/toys/'
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
